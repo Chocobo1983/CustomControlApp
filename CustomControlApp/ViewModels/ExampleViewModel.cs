bf90f7766d26e609ce3c5f5aff68773b4618b6b7
stb.infoserv <stb.infoserv@gmail.com>
2022-01-03 14:40:20 +0200

    added random angle changes

---
+++ CustomControlApp/ViewModels/ExampleViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CustomControlApp.ViewModels
+{
+    internal class ExampleViewModel : VMBase
+    {
+        int _counter = 0;
+        double _angle = 0;
+        public int Counter { get => _counter ;  set { _counter = value; OnPropertyChanged(nameof(Counter)); } }
+        public double Angle { get => _angle; set { _angle = value; OnPropertyChanged(nameof(Angle)); } }
+        public ICommand RandomAngleCommand => new RelayCommand((object o) => Angle = new Random().Next(0, 360));
+    }
+}
---
+++ CustomControlApp/ViewModels/RotationManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace CustomControlApp.ViewModels
+{
+    internal class RotationManager : DependencyObject
+    {
+        public static readonly DependencyProperty AngleProperty = DependencyProperty.RegisterAttached("Angle",
+           typeof(double), typeof(RotationManager), new UIPropertyMetadata(0.0, OnAngleChanged));
+        public static double GetAngle(DependencyObject d) => (double)d.GetValue(AngleProperty); 
+        public static void SetAngle(DependencyObject d, double value) => d.SetValue(AngleProperty, value);
+        private static void OnAngleChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            var Element = obj as UIElement;
+            if (Element != null)
+            {
+                Element.RenderTransformOrigin = new Point(.5, .5);
+                Element.RenderTransform = new RotateTransform((double)e.NewValue);
+            }
+        }
+        
+    }   
+}
